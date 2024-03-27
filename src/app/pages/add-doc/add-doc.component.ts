029b9cc5c1f68cbaf8c181dae9024f7c92c1cb79
limhokyun <lim@nsmarts.co.kr>
2024-03-27 11:38:52 +0900

    first

---
+++ src/app/pages/add-doc/add-doc.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-doc',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-doc.component.html',
+  styleUrl: './add-doc.component.scss'
+})
+export class AddDocComponent {
+
+}
