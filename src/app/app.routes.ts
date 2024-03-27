502329874e5f96258a319e1b70e45d431cfa7eda
limhokyun <lim@nsmarts.co.kr>
2024-03-27 14:18:24 +0900

    [feat] 기본 레이아웃 구성

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,24 @@
 import { Routes } from '@angular/router';
+import { ChatComponent } from './pages/chat/chat.component';
+import { AddDocComponent } from './pages/add-doc/add-doc.component';
+import { DocListComponent } from './pages/doc-list/doc-list.component';
 
 export const routes: Routes = [
-
+    {
+        path: '',
+        title: 'chat',
+        component: ChatComponent
+    },
+    {
+        path: "add_doc",
+        title: 'add_doc',
+        component: AddDocComponent
+    }, {
+        path: "doc_list",
+        title: "doc_list",
+        component: DocListComponent
+    }, {
+        path: "*",
+        redirectTo: '/'
+    }
 ];
