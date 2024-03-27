9cd7fb52fde0fc5c95f8713bb9b1448465c80962
limhokyun <lim@nsmarts.co.kr>
2024-03-27 16:57:20 +0900

    [feat] drag and drop 처리

--- src/app/pages/add-doc/add-doc.component.ts
+++ src/app/pages/add-doc/add-doc.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DndDirective } from '../../directives/dnd.directive';
 @Component({
   selector: 'app-add-doc',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatIconModule, DndDirective],
   templateUrl: './add-doc.component.html',
   styleUrl: './add-doc.component.scss'
 })
 export class AddDocComponent {
+  // on file drop handler
+  onFileDropped($event: any) {
+    console.log($event)
+  }
 
+  // handle file from browsing
+  fileBrowseHandler(target: any) {
+    console.log(target.files)
+  }
 }
