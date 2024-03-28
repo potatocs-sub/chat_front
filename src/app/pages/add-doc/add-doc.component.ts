c7c6dd766845212e928a574d4cdb2a4bedfb693e
limhokyun <lim@nsmarts.co.kr>
2024-03-28 09:50:53 +0900

    [chore] 등록 폼 html,css 완성

--- src/app/pages/add-doc/add-doc.component.ts
+++ src/app/pages/add-doc/add-doc.component.ts
@@ -3,10 +3,11 @@ import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { DndDirective } from '../../directives/dnd.directive';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-add-doc',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, DndDirective],
+  imports: [CommonModule, MatButtonModule, MatIconModule, DndDirective, RouterModule],
   templateUrl: './add-doc.component.html',
   styleUrl: './add-doc.component.scss'
 })
