846b9a069f32f020c4117e08c36ad7bf259cbf56
limhokyun <lim@nsmarts.co.kr>
2024-04-02 10:47:03 +0900

    [feat] 채팅 기능 구현

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule } from '@angular/router';
@@ -11,15 +11,16 @@ import { FormsModule } from '@angular/forms';
   standalone: true,
   imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule, FormsModule],
   templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss'
+  styleUrl: './chat.component.scss',
+  //https://stackoverflow.com/questions/44210786/style-not-working-for-innerhtml-in-angular
+  encapsulation: ViewEncapsulation.None,
 })
 export class ChatComponent {
   history: Array<any> = []; // 대화 내역 저장용 
   qustion: string = ''; // 질문 
-  company: string = ''; // 회사 
+  company: string = 'nsmarts'; // 회사 
 
   constructor(private chatService: ChatService) { }
-
 
 
   /**
@@ -27,7 +28,11 @@ export class ChatComponent {
    */
   submit() {
     this.chatService.ask(this.qustion, this.history, this.company).subscribe((res: any) => {
-      console.log(res);
+      if (res.status) {
+        this.history.push('<article class="answer">' + res.answer.kwargs.content + '</article>')
+      }
     })
+    this.history.push('<article class="question">' + this.qustion + '</article>');
+    this.qustion = '';
   }
 }
