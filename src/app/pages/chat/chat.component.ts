fa0a302851356a2aa669510cac63193dd9ad9802
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-12-19 15:56:29 +0900

    submit 설명 추가가

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -7,38 +7,43 @@ import { ChatService } from '../../services/chat.service';
 import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-chat',
-  standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule, FormsModule],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.scss',
-  //https://stackoverflow.com/questions/44210786/style-not-working-for-innerhtml-in-angular
-  encapsulation: ViewEncapsulation.None,
+    selector: 'app-chat',
+    standalone: true,
+    imports: [CommonModule, MatButtonModule, MatIconModule, RouterModule, FormsModule],
+    templateUrl: './chat.component.html',
+    styleUrl: './chat.component.scss',
+    //https://stackoverflow.com/questions/44210786/style-not-working-for-innerhtml-in-angular
+    encapsulation: ViewEncapsulation.None,
 })
 export class ChatComponent {
-  history: Array<any> = []; // 대화 내역 저장용 
-  qustion: string = ''; // 질문 
-  company: string = 'nsmarts'; // 회사 
+    history: Array<any> = []; // 대화 내역 저장용 
+    qustion: string = ''; // 질문 
+    company: string = 'nsmarts'; // 회사 
 
-  waiting: boolean = false;
+    waiting: boolean = false;
 
-  constructor(private chatService: ChatService) { }
+    constructor(private chatService: ChatService) { }
 
 
-  /**
-   * 질문용 함수 
-   */
-  submit() {
-    if (this.waiting) return;
+    submit() {
+        // 이미 요청 중이라면 실행하지 않음
+        if (this.waiting) return;
 
-    this.waiting = true;
-    this.chatService.ask(this.qustion, this.history, this.company).subscribe((res: any) => {
-      if (res.status) {
-        this.history.push('<article class="answer">' + res.answer.kwargs.content + '</article>')
-      }
-      this.waiting = false;
-    })
-    this.history.push('<article class="question">' + this.qustion + '</article>');
-    this.qustion = '';
-  }
+        this.waiting = true; // 요청 대기 상태로 설정
+
+        // chatService를 통해 질문, 히스토리, 회사 정보를 서버에 전달
+        this.chatService.ask(this.qustion, this.history, this.company).subscribe((res: any) => {
+            // 응답이 성공적일 경우
+            if (res.status) {
+                // 응답 내용을 히스토리에 추가 (답변 형태)
+                this.history.push('<article class="answer">' + res.answer.kwargs.content + '</article>');
+            }
+            this.waiting = false; // 요청 완료 상태로 설정
+        });
+
+        // 사용자가 입력한 질문을 히스토리에 추가 (질문 형태)
+        this.history.push('<article class="question">' + this.qustion + '</article>');
+        this.qustion = ''; // 입력 필드 초기화
+    }
+
 }
